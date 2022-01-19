520288f9fb17411af84ec55cca59781e9319d3d2
Will Darley <16darlew@fulford.york.sch.uk>
2022-01-19 10:11:48 +0000

    Finished prototype 1.1a

--- String comparer/Form1.Designer.cs
+++ String comparer/Form1.Designer.cs
@@ -28,20 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.tbxStringX = new System.Windows.Forms.TextBox();
+            this.tbxStringY = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(37, 447);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(197, 77);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            // 
+            // tbxStringX
+            // 
+            this.tbxStringX.Location = new System.Drawing.Point(37, 184);
+            this.tbxStringX.Multiline = true;
+            this.tbxStringX.Name = "tbxStringX";
+            this.tbxStringX.Size = new System.Drawing.Size(238, 40);
+            this.tbxStringX.TabIndex = 1;
+            // 
+            // tbxStringY
+            // 
+            this.tbxStringY.Location = new System.Drawing.Point(37, 284);
+            this.tbxStringY.Multiline = true;
+            this.tbxStringY.Name = "tbxStringY";
+            this.tbxStringY.Size = new System.Drawing.Size(238, 59);
+            this.tbxStringY.TabIndex = 3;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(1011, 76);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(116, 39);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.Text = "Result";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(97, 19);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(538, 55);
+            this.lblTitle.TabIndex = 5;
+            this.lblTitle.Text = "String overlap calculator";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1295, 751);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.tbxStringY);
+            this.Controls.Add(this.tbxStringX);
+            this.Controls.Add(this.btnCalc);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "String Comparer";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox tbxStringX;
+        private System.Windows.Forms.TextBox tbxStringY;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblTitle;
     }
 }
 
